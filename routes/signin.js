a66949ebc05b89bf3055d4418d8186ea53b068c0
ANTITRIX <102489946+ANTITRIX@users.noreply.github.com>
2024-08-08 22:31:58 +0300

    Update signin.js

--- routes/signin.js
+++ routes/signin.js
@@ -4,49 +4,6 @@ const bcrypt = require('bcrypt');
 const User = require('../models/Users');
 const {renderSignin,handleSignin} = require('../controllers/authControllers');
 let message=''
-
-
-/*
-router.get('/', (req, res) => {
-    res.render('signin',{ message });
-});
-
-// Handle login form submission
-router.post("/", async (req, res) => {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-        return res.status(400).send('Email and password are required.');
-    }
-
-    try {
-        const user = await User.findOne({ where: { email } })
-        if (!user) {
-            message="this email does not have an associated account.";
-            return res.render('signin',{ message });
-        }
-
-
-
-        if (!(password===user.password)) {
-            message="Authentication failed. Please check your credentials.";
-            return res.render('signin',{ message });
-        } else {
-            req.session.isAuth=true;
-            req.session.user=user;
-            await req.session.save();
-            console.log(req.session)
-            return res.redirect('/profile')
-        }
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Server error' });
-    }
-});
-
-module.exports = router;
-*/
-
 router.get('/',renderSignin);
 router.post('/',handleSignin);
 module.exports=router;
